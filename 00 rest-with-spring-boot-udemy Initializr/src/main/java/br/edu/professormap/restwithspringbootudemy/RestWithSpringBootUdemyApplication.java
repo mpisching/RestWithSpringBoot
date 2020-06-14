a4241c5365258377acae04c9b13cb2ca5563f830
Marcos Pisching <mpisching@gmail.com>
2020-06-14 12:13:58 -0300

    Hello SpringBoot and REST Udemy

---
+++ 00 rest-with-spring-boot-udemy Initializr/src/main/java/br/edu/professormap/restwithspringbootudemy/RestWithSpringBootUdemyApplication.java
@@ -0,0 +1,13 @@
+package br.edu.professormap.restwithspringbootudemy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestWithSpringBootUdemyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestWithSpringBootUdemyApplication.class, args);
+	}
+
+}
